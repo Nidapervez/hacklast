1973a48d1ab23bb474d2565f71021c27f102e168
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2025-02-04 10:15:48 +0500

    Update header2.tsx

--- src/app/components/header2.tsx
+++ src/app/components/header2.tsx
@@ -51,7 +51,7 @@ const Header1 = () => {
               <FontAwesomeIcon icon={faShoppingCart} className="mr-2" /> Cart
             </button>
           </Link>
-          <Link href="/dashboard">
+          <Link href="https://dashborad-tawny.vercel.app/">
             <button className="flex items-center">
               <FontAwesomeIcon icon={faCogs} className="mr-2" /> Admin Dashboard
             </button>
